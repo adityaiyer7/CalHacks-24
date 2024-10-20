5270b6908fe39a6697781abc84c44176b0b66dac
Hoang Anh Tran <htran31@sfsu.edu>
2024-10-20 10:18:52 -0700

    added features section on landing page

--- baby-tracker/app/page.tsx
+++ baby-tracker/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaDiscord } from "react-icons/fa";
@@ -11,9 +11,47 @@ import sleepGif from "/public/images/sleep.gif";
 import feedGif from "/public/images/feed.gif";
 import growthGif from "/public/images/growth.gif";
 import logo from "/public/images/logo.png";
+import feature1 from "/public/images/feature1.png"; // Replace with actual image paths
+import feature2 from "/public/images/feature2.png"; // Replace with actual image paths
+import feature3 from "/public/images/feature3.png"; // Replace with actual image paths
 
 const Home: React.FC = () => {
   const router = useRouter();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  const features = [
+    {
+      title: "Feature 1",
+      description: "Description of feature 1.",
+      image: feature1,
+    },
+    {
+      title: "Feature 2",
+      description: "Description of feature 2.",
+      image: feature2,
+    },
+    {
+      title: "Feature 3",
+      description: "Description of feature 3.",
+      image: feature3,
+    },
+  ];
+
+  // Handle automatic slide every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % features.length);
+    }, 5000);
+    return () => clearInterval(interval); // Cleanup the interval
+  }, [features.length]);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % features.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + features.length) % features.length);
+  };
 
   const navigateToLogin = () => {
     router.push("/dashboard"); // Navigate to Login
@@ -63,6 +101,34 @@ const Home: React.FC = () => {
         </div>
       </section>
 
+      <section id="features" className="flex flex-col items-center min-w-[95vh] min-h-[95vh] p-10 rounded-lg text-center mt-10">
+        <h2 className="text-white text-5xl font-bold mb-5 mt-10">Our Features</h2>
+        <p className="text-white text-xl">Why you need this baby tracker</p>
+        <div className="relative w-[150%] overflow-hidden rounded-lg">
+          <div className="relative w-full flex justify-center">
+            <div className="w-[90%]">
+              <Image
+                src={features[currentIndex].image}
+                alt={features[currentIndex].title}
+                width={1800}
+                height={1350}
+                className="w-full h-auto"
+              />
+              {/* <h3 className="text-2xl font-bold mt-5">{features[currentIndex].title}</h3> */}
+              <p className="text-gray-600">{features[currentIndex].description}</p>
+            </div>
+          </div>
+
+          {/* Navigation buttons */}
+          <button onClick={handlePrev} className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-[#E2F6FF] p-5 rounded-full">
+            &#8249;
+          </button>
+          <button onClick={handleNext} className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-[#E2F6FF] p-5 rounded-full">
+            &#8250;
+          </button>
+        </div>
+      </section>
+
       <section id="trackers" className="bg-[#27595A] p-10 rounded-lg text-center mt-10">
         <h2 className="text-white text-5xl font-bold mb-5">Our Core Trackers</h2>
         <p className="text-white text-lg mb-5">
@@ -70,29 +136,27 @@ const Home: React.FC = () => {
           Discover how our voice AI technology makes tracking easier and more intuitive!
         </p>
         <div className="flex justify-around flex-wrap">
-          {[
-            {
-              title: "Feed Tracker",
-              image: feedGif,
-              description: "Track feeding schedules and amounts effortlessly using your voice.",
-              bgColor: "bg-[#E2F6FF]", // Feed Tracker background
-              imageClass: "px-6",
-            },
-            {
-              title: "Sleep Tracker",
-              image: sleepGif,
-              description: "Monitor sleep patterns and durations with ease through voice commands.",
-              bgColor: "bg-[#FCC035]", // Sleep Tracker background
-              imageClass: "mt-0",
-            },
-            {
-              title: "Growth Tracker",
-              image: growthGif,
-              description: "Log growth milestones and health updates without the hassle of typing.",
-              bgColor: "bg-[#44BFAB]", // Growth Tracker background
-              imageClass: "mt-0",
-            },
-          ].map((feature, index) => (
+          {[{
+            title: "Feed Tracker",
+            image: feedGif,
+            description: "Track feeding schedules and amounts effortlessly using your voice.",
+            bgColor: "bg-[#E2F6FF]", // Feed Tracker background
+            imageClass: "px-6",
+          },
+          {
+            title: "Sleep Tracker",
+            image: sleepGif,
+            description: "Monitor sleep patterns and durations with ease through voice commands.",
+            bgColor: "bg-[#FCC035]", // Sleep Tracker background
+            imageClass: "mt-0",
+          },
+          {
+            title: "Growth Tracker",
+            image: growthGif,
+            description: "Log growth milestones and health updates without the hassle of typing.",
+            bgColor: "bg-[#44BFAB]", // Growth Tracker background
+            imageClass: "mt-0",
+          }].map((feature, index) => (
             <div key={index} className={`${feature.bgColor} rounded-lg p-10 m-10 shadow-md flex flex-col items-center w-80 mb-20`}>
               <h3 className="text-black text-xl font-bold mb-5">{feature.title}</h3>
               <Image src={feature.image} alt={feature.title} width={200} height={200} className={feature.imageClass} />
@@ -102,23 +166,14 @@ const Home: React.FC = () => {
         </div>
       </section>
 
-
       <footer className="mt-auto p-10 text-center text-white bg-[#27595A] w-full border-t border-[#E2F6FF]">
         <p>BabyBytes &copy; CalHack{new Date().getFullYear()}</p>
         <p>All rights reserved.</p>
         <div className="flex justify-center gap-5 mt-5">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <FaFacebookF size={24} />
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <FaInstagram size={24} />
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <FaLinkedinIn size={24} />
-          </a>
-          <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
-            <FaDiscord size={24} />
-          </a>
+          <FaFacebookF className="hover:text-[#FCC035] cursor-pointer" />
+          <FaInstagram className="hover:text-[#FCC035] cursor-pointer" />
+          <FaLinkedinIn className="hover:text-[#FCC035] cursor-pointer" />
+          <FaDiscord className="hover:text-[#FCC035] cursor-pointer" />
         </div>
       </footer>
     </div>
